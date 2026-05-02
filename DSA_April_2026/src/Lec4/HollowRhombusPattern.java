32aa3ddc846e6251dfcf63b17b94724663c93b77
Sonu Yagyasaini <SonuYagyasaini@outlook.com>
2026-05-02 16:39:30 +0530

    Pattern rombus assignment

---
+++ DSA_April_2026/src/Lec4/HollowRhombusPattern.java
@@ -0,0 +1,40 @@
+package Lec4;
+import java.util.*;
+public class HollowRhombusPattern {
+
+	public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int space = n-1;
+        int star = n;
+        int row = 1;
+        while(row <= n)
+        {
+            // forming space here...
+            int i = 1;
+            while(i <= space)
+            {
+                System.out.print(" ");
+                i++;
+            }
+            // forming star here...
+            int j = 1;
+            while(j <= star)
+            {
+                if(j == 1 || j == star || row == 1 || row == n)
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                } 
+                j++;
+            }
+            System.out.println();
+            row++;
+            space--;
+        }
+    }
+
+}
