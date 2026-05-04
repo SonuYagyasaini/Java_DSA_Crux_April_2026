c9d9e87f37bd649a79ead2db45801995997d872a
Sonu Yagyasaini <SonuYagyasaini@outlook.com>
2026-05-04 20:21:28 +0530

    Decimal to binary conversion

---
+++ DSA_April_2026/src/Lec5/DecimalToBinary.java
@@ -0,0 +1,20 @@
+package Lec5;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+		int n = 37;
+		int sum = 0;
+		int mul = 1;
+		while(n > 0)
+		{
+			int rem = n%2;
+			sum = sum+rem*mul;
+			mul *= 10;
+			n = n/2;
+		}
+		System.out.println(sum);
+
+	}
+
+}
